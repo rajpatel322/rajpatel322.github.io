91ba93ff0ca7e5359a45eaf489b0d902453ad36c
rajpatel322 <rajkpatel10@gmail.com>
2021-03-10 09:51:51 -0600

    Changes

--- Back-End/triksnas/settings.py
+++ Back-End/triksnas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd1)a0+u-z+r_x_=%xiw5d#i&8!h$7r=gm-3y=bgj80oc)hvs_0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
