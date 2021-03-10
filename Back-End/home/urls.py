664b3502b9578c8eb181b02cf1ed554ca34778b8
rajpatel322 <rajkpatel10@gmail.com>
2021-03-10 13:00:46 -0600

    more changes

--- Back-End/home/urls.py
+++ Back-End/home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from home import views
 urlpatterns = [
    path('', views.home, name='home'),
-   path('about', views.about, name='about'),
+   path('project', views.project, name='project'),
+   path('contact', views.contact, name='contact'),
 ]
--- Back-End/home/views.py
+++ Back-End/home/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def home(request):
-    #return HttpResponse("this is the home")
+    # return HttpResponse("this is the home")
     return render(request, 'home.html')
 
-def about(request):
-    #return HttpResponse("this is the home")
-    return render(request, 'index.html')
+
+def project(request):
+    # return HttpResponse("this is the home")
+    return render(request, 'project.html')
+
+
+def contact(request):
+    # return HttpResponse("this is the home")
+    return render(request, 'contact.html')
