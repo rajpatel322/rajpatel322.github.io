5d3470696e32700626fc43d8d87b10603347d29c
rajpatel322 <rajkpatel10@gmail.com>
2021-03-10 16:27:06 -0600

    heroku

--- Back-End/triksnas/settings.py
+++ Back-End/triksnas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
