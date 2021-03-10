4ab67dcecdeb5abe76acbef830a5a78b6b0c6f98
rajpatel322 <rajkpatel10@gmail.com>
2021-03-10 12:05:44 -0600

    connect the /about request to the index/html file

--- Back-End/home/views.py
+++ Back-End/home/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("this is the home")
+    #return HttpResponse("this is the home")
+    return render(request, 'home.html')
+
+def about(request):
+    #return HttpResponse("this is the home")
+    return render(request, 'index.html')
