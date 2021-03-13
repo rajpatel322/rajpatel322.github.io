0cdf1bc498c60c17ed82d49e136f65423e7e113f
rajpatel322 <rajkpatel10@gmail.com>
2021-03-13 03:31:53 -0600

    Update the file

--- BlackJack/triksnas/settings.py
+++ BlackJack/triksnas/settings.py
@@ -119,11 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/images/'
-STATICFILES_DIRS =[]
+STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-
