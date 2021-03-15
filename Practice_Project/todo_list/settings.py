72be350f45dc769ce6675d4003d41af5610c1947
rajpatel322 <rajkpatel10@gmail.com>
2021-03-15 18:03:41 -0500

    HIDING THE SECRET_KEY

--- Practice_Project/todo_list/settings.py
+++ Practice_Project/todo_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h87=$3rs@e3+!=1%$%878w&1m_8*j9yo+pr3z0onors1g0kjyb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -114,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOGIN_URL ='/login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
