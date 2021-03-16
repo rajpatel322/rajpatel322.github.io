81d4f332c5e968ce466bb56fde0d5a794e6913cf
rajpatel322 <rajkpatel10@gmail.com>
2021-03-16 01:23:22 -0500

    Desiging

--- Practice_Project/base/views.py
+++ Practice_Project/base/views.py
@@ -73,7 +73,7 @@ class TaskCreate(LoginRequiredMixin,CreateView):
 
 class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
-    fields = ['Title', 'Description', 'Complete']
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 class DeleteView(LoginRequiredMixin,DeleteView):
